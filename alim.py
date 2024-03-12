ce6c3faeab42b8b58288799f4eae1bc955af1bcd
salvaCasta276 <salCas276@gmail.com>
2024-03-12 20:40:01 -0300

    Added some value printing and did some refactoring...

--- alim.py
+++ alim.py
@@ -9,10 +9,12 @@ sheet = xls.parse(0)
 sheet = np.array(sheet)
 numsheet, genders = np.split(sheet, [-1], axis=1)
 numsheet = numsheet.astype(float)
+index = ['Grasas_sat', 'Alcohol', 'Calorías']
 
 #Replace not found values
 numsheet[np.isin(numsheet, [999.99])] = np.nan
 mean = np.nanmean(numsheet, axis=0)
+std = np.nanstd(numsheet, axis=0)
 
 numsheet[:,0][np.isnan(numsheet[:,0])] = mean[0]
 numsheet[:,1][np.isnan(numsheet[:,1])] = mean[1]
@@ -25,9 +27,14 @@ numsheet[:,2][np.isnan(numsheet[:,2])] = mean[2]
 # df['Calorías'].fillna(df['Calorías'].mode().iloc[0], inplace=True)
 
 #General normalization
-normsheet = (numsheet - mean) / np.nanstd(numsheet, axis=0)
+normsheet = (numsheet - mean) / std
 
-df = pd.DataFrame(normsheet, columns=['Grasas_sat', 'Alcohol', 'Calorías'])
+print("General means:")
+print(mean)
+print("General stds:")
+print(std)
+
+df = pd.DataFrame(normsheet, columns=index)
 df.boxplot()
 
 plt.title('Boxplot General')
@@ -36,7 +43,7 @@ plt.show()
 
 #Bar graphs (normalized)
 
-df = pd.DataFrame(normsheet, columns=['Grasas_sat', 'Alcohol', 'Calorías'])
+df = pd.DataFrame(normsheet, columns=index)
 df.plot.hist()
 
 plt.ylabel('Values')
@@ -44,28 +51,53 @@ plt.title('Bar Graph')
 plt.show()
 
 #Sex dependent normalization 
-
 sheet = np.concatenate((numsheet, genders), axis=1)
 
-Ms = sheet[sheet[:, 3] == 'M', :3].astype(float)
-Fs = sheet[sheet[:, 3] == 'F', :3].astype(float)
-Ms = (Ms- np.nanmean(Ms, axis=0)) / np.nanstd(Ms, axis=0)
-Fs = (Fs- np.nanmean(Fs, axis=0)) / np.nanstd(Fs, axis=0)
+female= sheet[sheet[:, 3] == 'F', :3].astype(float)
+male = sheet[sheet[:, 3] == 'M', :3].astype(float)
 
+f_mean = np.nanmean(female, axis=0)
+f_std = np.nanstd(female, axis=0)
+m_mean = np.nanmean(male, axis=0)
+m_std = np.nanstd(male, axis=0)
 
-df = pd.DataFrame(Ms, columns=['Grasas_sat', 'Alcohol', 'Calorías'])
+female = (female - f_mean) / np.nanstd(female, axis=0)
+male = (male - m_mean) / np.nanstd(male, axis=0)
+
+print("Female means:")
+print(f_mean)
+print("Female stds:")
+print(f_std)
+
+df = pd.DataFrame(female, columns=index)
 df.boxplot()
 
 plt.title('Boxplot Mujeres')
 plt.ylabel('Valores Normalizados')
 plt.show()
 
-df = pd.DataFrame(Fs, columns=['Grasas_sat', 'Alcohol', 'Calorías'])
+print("Male means:")
+print(m_mean)
+print("Male stds:")
+print(m_std)
+
+df = pd.DataFrame(male, columns=index)
 df.boxplot()
 
 plt.title('Boxplot Hombres')
 plt.ylabel('Valores Normalizados')
 plt.show()
+
+print('Comparacion Medias Hombres:')
+print((mean-m_mean)/mean)
+print('Comparacion Medias Mujeres:')
+print((mean-f_mean)/mean)
+
+#df = pd.DataFrame({'Media Hombre': (mean-m_mean)/mean, 'Media Mujer': (mean-f_mean)/mean}, index=index)
+#df.plot.bar(rot=0)
+#plt.title('Media Hombre-Mujer')
+#plt.ylabel('Valores Normalizados')
+#plt.show()
 
 # Alcohol vs calories
 
