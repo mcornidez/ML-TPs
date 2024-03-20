b3af6934b2f88da9f41772026eca6f1fbfe94acc
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2024-03-20 11:02:59 -0300

    fix multiple linear regresion

--- Ejercicio Obligatorio 2/main.py
+++ Ejercicio Obligatorio 2/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def variable_correlation(x):
     cov = np.cov(x)
     std = x.std(axis=1)
@@ -33,7 +34,9 @@ def simple_linear_regresion(x, y, x_label="...", y_label="Sales", title="..."):
     r2 = modelSquareSum / totalSquareSum
 
     print(f"R2 = {r2}")
-    print(f"Is close: {np.isclose(statistics.covariance(predicted_y, y - predicted_y), 0)}")
+    print(
+        f"Is close: {np.isclose(statistics.covariance(predicted_y, y - predicted_y), 0)}"
+    )
 
     # NOTE: Draw data
     plt.scatter(x, y)
@@ -48,47 +51,67 @@ def simple_linear_regresion(x, y, x_label="...", y_label="Sales", title="..."):
     plt.show()
 
     plt.scatter(predicted_y, y - predicted_y)
-    plt.hlines(y=[0], xmin=[predicted_y.min()], xmax=[predicted_y.max()], colors='grey', linestyles='--')
-    plt.xlabel('Predicted ' + y_label)
-    plt.ylabel('Residual')
+    plt.hlines(
+        y=[0],
+        xmin=[predicted_y.min()],
+        xmax=[predicted_y.max()],
+        colors="grey",
+        linestyles="--",
+    )
+    plt.xlabel("Predicted " + y_label)
+    plt.ylabel("Residual")
     plt.show()
 
     return (a, b)
 
 
 def multiple_linear_regresion(x, y):
-    cov_xy = np.array([statistics.covariance(xi, y) for xi in x])
-    var_x = x.var(axis=1, ddof = 1)
+    # cov_xy = np.array([statistics.covariance(xi, y) for xi in x])
+    # var_x = x.var(axis=1, ddof=1)
+
+    # b = cov_xy / var_x
+
+    # a = mean_y - np.dot(b, mean_x)
+
+    # predicted_y = a + np.dot(x.transpose(), b)
+
+    x = np.append(np.ones((1, x.shape[1])), x, axis=0).T
+
+    b = np.linalg.inv(x.T @ x) @ x.T @ y
+
+    predicted_y = x @ b
 
     mean_x = x.mean(axis=1)
     mean_y = y.mean()
-
-    b = cov_xy / var_x
-
-    a = mean_y - np.dot(b, mean_x)
-
-    predicted_y = a + np.dot(x.transpose(), b)
 
     totalSquareSum = sum((y - mean_y) ** 2)
     modelSquareSum = sum((predicted_y - mean_y) ** 2)
     resudueSquareSum = sum((y - predicted_y) ** 2)
 
     r2 = modelSquareSum / totalSquareSum
-    #Para que se de la igualdad esto de abajo deberia ser cercado a 0, como pasa en los casos unidimensionales
-    #Explicado en respuesta a este post
-    #https://stats.stackexchange.com/questions/265869/confused-with-residual-sum-of-squares-and-total-sum-of-squares
-    print(f"Is close: {np.isclose(statistics.covariance(predicted_y, y - predicted_y), 0)}")
+    # Para que se de la igualdad esto de abajo deberia ser cercado a 0, como pasa en los casos unidimensionales
+    # Explicado en respuesta a este post
+    # https://stats.stackexchange.com/questions/265869/confused-with-residual-sum-of-squares-and-total-sum-of-squares
+    print(
+        f"Is close: {np.isclose(statistics.covariance(predicted_y, y - predicted_y), 0)}"
+    )
 
     print(f"R2 = {r2}")
     print(f"R2' = {1 - resudueSquareSum / totalSquareSum}")
 
     plt.scatter(predicted_y, y - predicted_y)
-    plt.hlines(y=[0], xmin=[predicted_y.min()], xmax=[predicted_y.max()], colors='grey', linestyles='--')
-    plt.xlabel('Predicted Sales')
-    plt.ylabel('Residual')
+    plt.hlines(
+        y=[0],
+        xmin=[predicted_y.min()],
+        xmax=[predicted_y.max()],
+        colors="grey",
+        linestyles="--",
+    )
+    plt.xlabel("Predicted Sales")
+    plt.ylabel("Residual")
     plt.show()
 
-    return (a, b)
+    return b
 
 
 def main():
@@ -110,7 +133,9 @@ def main():
         newspaper, sales, title="Sales vs Newspaper", x_label="Newspaper"
     )
 
-    (a_all, b_all) = multiple_linear_regresion(x, sales)
+    bs_all = multiple_linear_regresion(x, sales)
+    a_all = bs_all[0]
+    bs_all = bs_all[1:]
 
 
 if __name__ == "__main__":
