47e42e315e6617e2b5235aad94593db0e9f896f5
salvaCasta276 <salCas276@gmail.com>
2024-03-20 00:15:43 -0300

    Added correlation calculation between variables

--- Ejercicio Obligatorio 2/main.py
+++ Ejercicio Obligatorio 2/main.py
@@ -5,6 +5,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def variable_correlation(x):
+    cov = np.cov(x)
+    std = x.std(axis=1)
+
+    print(f"Corr TV - Radio: {cov[0,1]/(std[0]*std[1])}")
+    print(f"Corr TV - Newspaper: {cov[0,2]/(std[0]*std[2])}")
+    print(f"Corr Radio - Newspaper: {cov[1,2]/(std[1]*std[2])}")
+
 
 def simple_linear_regresion(x, y, x_label="...", y_label="Sales", title="..."):
     cov_xy = statistics.covariance(x, y)
@@ -45,31 +53,27 @@ def simple_linear_regresion(x, y, x_label="...", y_label="Sales", title="..."):
 
 def multiple_linear_regresion(x, y):
     cov_xy = np.array([statistics.covariance(xi, y) for xi in x])
-    var_x = np.var(x, axis=1, ddof = 1)
+    var_x = x.var(axis=1, ddof = 1)
 
     mean_x = x.mean(axis=1)
     mean_y = y.mean()
-
-    print(statistics.covariance(x[0], x[1]))
-    print(statistics.covariance(x[1], x[2]))
-    print(statistics.covariance(x[0], x[2]))
 
     b = cov_xy / var_x
 
     a = mean_y - np.dot(b, mean_x)
 
-    predicted_y = a + np.dot(np.transpose(x), b)
+    predicted_y = a + np.dot(x.transpose(), b)
 
     totalSquareSum = sum((y - mean_y) ** 2)
     modelSquareSum = sum((predicted_y - mean_y) ** 2)
     resudueSquareSum = sum((y - predicted_y) ** 2)
 
     r2 = modelSquareSum / totalSquareSum
-    print(1 - resudueSquareSum / totalSquareSum)
     #Para que se de la igualdad esto de abajo deberia ser cercado a 0, como pasa en los casos unidimensionales
     print(f"Is close: {np.isclose(statistics.covariance(predicted_y, y - predicted_y), 0)}")
 
     print(f"R2 = {r2}")
+    print(f"R2' = {1 - resudueSquareSum / totalSquareSum}")
 
     return (a, b)
 
@@ -82,7 +86,9 @@ def main():
     tv = df["TV"].to_numpy()
     radio = df["Radio"].to_numpy()
     newspaper = df["Newspaper"].to_numpy()
+    x = np.array([tv, radio, newspaper])
 
+    variable_correlation(x)
     (a_tv, b_tv) = simple_linear_regresion(tv, sales, title="Sales vs TV", x_label="TV")
     (a_radio, b_radio) = simple_linear_regresion(
         radio, sales, title="Sales vs Radio", x_label="Radio"
@@ -91,7 +97,7 @@ def main():
         newspaper, sales, title="Sales vs Newspaper", x_label="Newspaper"
     )
 
-    (a_all, b_all) = multiple_linear_regresion(np.array([tv, radio, newspaper]), sales)
+    (a_all, b_all) = multiple_linear_regresion(x, sales)
 
 
 if __name__ == "__main__":
