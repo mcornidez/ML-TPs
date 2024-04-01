6bac434a0624fbac6f1c0e09af45d7d9c35d5e98
salvaCasta276 <salCas276@gmail.com>
2024-04-01 17:07:00 -0300

    Argument for specific data point implemented

--- TP1/ej1.py
+++ TP1/ej1.py
@@ -10,14 +10,21 @@ def main():
     prefs = df[["scones", "cerveza", "wiskey", "avena", "futbol"]].to_numpy()
     nac = df["Nacionalidad"].to_numpy()
 
+    inst = 1 - np.array([[0, 1, 1, 0, 1]])
     nac_set = set(nac)
     class_indexes = list(map(lambda x: np.where(nac == x)[0], nac_set))
-    counts = np.asarray(list(map(lambda x: x.size, class_indexes)))
+    counts = np.array(list(map(lambda x: x.size, class_indexes)))
     total_count = counts.sum()
     class_probs = counts / total_count
-    dissected_data = np.asarray(list(map(lambda x: prefs[x].transpose().sum(axis=1), class_indexes)))
+    #Hay que cambiar esto si se quieren consdierar mas condiciones que 0 y 1
+    dissected_data = np.array(list(map(lambda x: prefs[x].transpose().sum(axis=1), class_indexes)))
     indiv_prob = dissected_data/counts.reshape((2,1))
-    probs = indiv_prob.prod(axis=1) * class_probs
+
+    print(indiv_prob)
+    print(inst - indiv_prob)
+
+
+    probs = np.absolute((inst - indiv_prob ).prod(axis=1)) * class_probs
     final_probs = list(map(lambda x: x/probs.sum(), probs))
     print(nac_set)
     print(class_indexes)
