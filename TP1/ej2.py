77b0660dc167a1a67c8a9c572eb1a06a0bd9b928
salvaCasta276 <salCas276@gmail.com>
2024-04-07 19:47:20 -0300

    Implemented confusion matrix

--- TP1/ej2.py
+++ TP1/ej2.py
@@ -13,7 +13,7 @@ def main():
 
     #Por como estan ordenados los datos en este data set tengo una cantidad bien balanceadad de datos de cada categoria
     n = 34668
-    perc = 0.80
+    perc = 0.95
     threshold = 0.1
 
     data = df[["titular", "categoria"]].to_numpy()
@@ -28,16 +28,20 @@ def main():
 
     proc = time.time()
     conditional = ConditionalModel(binary_matrix[:int(n*perc)], categorias[:int(n*perc)])
+    confusion = np.zeros((len(CATS), len(CATS)))
     res = 0
     for i in range(int(n*perc), int(n)):
         classified = np.array(conditional.naive_classify(binary_matrix[i]))
-        res += (1 if classified.argmax(axis=0) - categorias[i] != 0 else 0)
+        max_index = classified.argmax()
+        confusion[categorias[i]][max_index] += 1
+        res += (1 if max_index - categorias[i] != 0 else 0)
 
     end = time.time()
 
     print("Accuracy percentage {}".format(1 - res/(n*(1-perc))))
     print("Preprocessing {}".format(proc-pre_proc))
     print("Processing {}".format(end-proc))
+    print(confusion)
 
 
 if __name__ == "__main__":
