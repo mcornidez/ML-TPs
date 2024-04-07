0a8a09e5183c992d5db59f8ca7abba7844a2f4f2
salvaCasta276 <salCas276@gmail.com>
2024-04-07 15:40:50 -0300

    Improvements on ex2

--- TP1/ej2.py
+++ TP1/ej2.py
@@ -8,14 +8,15 @@ import time
 CATS = [np.nan, 'Nacional', 'Ciencia y Tecnologia', 'Entretenimiento', 'Economia', 'Destacadas', 'Internacional', 'Deportes', 'Salud', 'Noticias destacadas']
 
 def main():
-    start = time.time()
-    df = pd.read_excel("Noticias_argentinas.xlsx")
+    pre_proc = time.time()
+    df = pd.read_excel("cropped_news.xlsx")
 
+    #Por como estan ordenados los datos en este data set tengo una cantidad bien balanceadad de datos de cada categoria
     n = 34668
     perc = 0.90
     threshold = 0.0
 
-    data = df[["titular", "categoria"]].to_numpy()[:n]
+    data = df[["titular", "categoria"]].to_numpy()
     np.random.shuffle(data)
     titles = data.transpose()[0]
     categorias = list(map(lambda x: CATS.index(x), data.transpose()[1]))
@@ -25,31 +26,18 @@ def main():
     binary_matrix = np.where(tfidf_matrix.toarray() > threshold, 1, 0)
     feature_names = tfidf_vectorizer.get_feature_names_out()
 
+    proc = time.time()
     conditional = ConditionalModel(binary_matrix[:int(n*perc)], categorias[:int(n*perc)])
     res = 0
     for i in range(int(n*perc), int(n)):
         classified = np.array(conditional.naive_classify(binary_matrix[i]))
-        #if i < n*perc+20:
-        #    print(classified)
-        #    print(classified.argmax(axis=0))
-        #    print(categorias[i])
         res += (1 if classified.argmax(axis=0) - categorias[i] != 0 else 0)
-    print(res/(n*(1-perc)))
 
     end = time.time()
 
-    print(end - start)
-
-    #for i, file in enumerate(titles):
-    #    if i > 20:
-    #        break
-    #    print(f"Words in file {i+1}:")
-    #    feature_index = tfidf_matrix[i,:].nonzero()[1]
-    #    tfidf_scores = zip(feature_index, [tfidf_matrix[i, x] for x in feature_index])
-    #    for word_index, score in tfidf_scores:
-    #        if score > 0.4:
-    #            print(f"{feature_names[word_index]}: {score}")
-
+    print("Accuracy percentage {}".format(1 - res/(n*(1-perc))))
+    print("Preprocessing {}".format(proc-pre_proc))
+    print("Processing {}".format(end-proc))
 
 
 
