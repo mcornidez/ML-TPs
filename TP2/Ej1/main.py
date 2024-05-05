79f4ace8c41a6c43aa802fcbbbc5e4c0ea45aa36
salvaCasta276 <salCas276@gmail.com>
2024-05-05 00:17:08 -0300

    Finished first version

--- TP2/Ej1/main.py
+++ TP2/Ej1/main.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
 import queue
+import math
 
 
 def main():
-    df = pd.read_csv("../Data/german_credit.csv", sep=";")
+    df = pd.read_csv("../Data/german_credit.csv", sep=",")
+    #Hay que volver a usar esas columnas una vez que se clasifique bien esa data
+    df = df.drop(['Duration of Credit (month)', 'Credit Amount', 'Age (years)'], axis=1)
     data = df.to_numpy()
-    print(data[:10])
-
+    tags = df.columns.to_list()
 
 if __name__ == "__main__":
     main()
@@ -15,13 +17,25 @@ if __name__ == "__main__":
 #Ver en que formato recibe la data de la variable y el dataset
 #Se espera que devuelva una fila con los gains
 def entropy_gain(variable, data):
-    pass
-    
+    total_len = len(data)
+    atribute_set = list(set(variable))
+    atribute_indexes = list(map(lambda x: np.where(variable == x), atribute_set))
+    classifications = list(map(lambda x: data[x], atribute_indexes)).insert(data, 0)
+
+    lens = np.array(list(map(lambda x: len(x), classifications)))
+    counts = np.array(list(map(lambda x: np.sum(x), classifications)))
+    probs = counts/lens
+    #Hay que usar el logaritmo con alguna base particular?
+    entropies = np.nan_to_num(probs * np.log(probs), 0) + np.nan_to_num((1-probs) * np.log(1-probs), 0)
+    entropies[0] = -entropies[0]
+    return np.sum(entropies * counts/total_len)
+
 
 #Se parte de que la variable con la que se clasifica esta en la primer posicion del arreglo
 class ID3:
-    def __init__(self, data, gain):
+    def __init__(self, data, tags, gain):
         self.data = data
+        self.tags = tags
         self.gain = gain
         self.root = self.TreeNode(None, 0, self.data)
         self.generate_tree()
@@ -40,29 +54,42 @@ class ID3:
                 if(len(set) == 0):
                     continue
 
-                child = self.TreeNode(current_node, id, current_node.depth+1, set)
+                child = self.TreeNode(current_node, id, current_node.depth+1, set, current_node.remaining_tags)
 
                 #Mirar si set[0] es fila o columna
                 if(not np.all(set[:, 0] == set[0][0])):
                     nodes.put(child)
 
                 current_node.add_child(child)
+    
+    def classify(self, point, point_tags):
+        current_node = self.root
+
+        while True:
+            if(len(current_node.children) == 0):
+                return current_node.get_classification()
+            subset_id = point[point_tags.index(current_node.tag)]
+            for child in current_node.children:
+                if child.subset_id == subset_id:
+                    current_node = child
 
     class TreeNode:
-        def __init__(self, father, subset_id, depth, data):
+        def __init__(self, father, subset_id, depth, data, remaining_tags):
             self.father = father
             self.subset_id = subset_id 
             self.depth = depth
             self.data = data 
+            self.remaining_tags = remaining_tags
+            self.tag = None
             self.children = []
             self.classification = None
             self.subsets = None
 
         def add_child(self, child):
             self.children.append(child)
-        
+
         # Le asigna el valor de la clase que mas aparezca en el arreglo de clases
-        def classify(self):
+        def get_classification(self):
             if(not self.classification is None):
                 return self.classification
 
@@ -75,10 +102,13 @@ class ID3:
         def partition_by_gain(self):
             if not self.subsets is None:
                 return self.subsets
-            gains = np.array(list(map(lambda x: entropy_gain(x, self.data[0]), self.data.transpose())))
+            gains = np.array(list(map(lambda x: entropy_gain(x, self.data[0]), self.data[:, 1:].transpose())))
             max_gain_index = np.argmax(gains)
+
             partition_col = self.data[:, max_gain_index]
             classification_set = list(set(partition_col))
+
+            self.tag = self.remaining_tags.pop(max_gain_index)
             cropped_data = np.delete(self.data, max_gain_index, index=1)
             partitions_indexes = list(map(lambda x: np.where(partition_col == x), classification_set))
             subsets = list(map(lambda x: cropped_data[x], partitions_indexes))
